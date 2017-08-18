a53dfbf529d5a53d914ad0a1bbd13e68509a0636
Fridolin Linder <fridolin.linder@gmail.com>
2017-08-18 09:14:49 -0400

    rename stream

---
+++ streaming.py
@@ -0,0 +1,36 @@
+#Import the necessary methods from tweepy library
+from __future__ import unicode_literals
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import re
+from credentials import credentials
+
+#Variables that contains the user credentials to access Twitter API 
+newline = re.compile(r'\n^')
+
+access_token = credentials["access_token"]
+access_token_secret = credentials["access_token_secret"]
+consumer_key = credentials["consumer_key"]
+consumer_secret = credentials["consumer_secret"]
+
+#This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print(data)
+        return True
+
+    def on_error(self, status):
+        print status
+
+if __name__ == '__main__':
+
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by the keywords
+    stream.filter(track=['Trump'])
